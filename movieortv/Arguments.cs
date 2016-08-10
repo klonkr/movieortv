aa672b0a3cda6e485273e6d37677b41997ef4cbe
Anton Cornett <anton.cornett@gmail.com>
2016-08-10 20:21:21 +0200

    implemented syntactically nicer command line arguments

---
+++ movieortv/Arguments.cs
@@ -0,0 +1,24 @@
+﻿using CommandLineParser.Arguments;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace movieortv
+{
+    class Arguments
+    {
+        [ValueArgument(typeof(string), 'i', "input", Description = "Input torrent name", ValueOptional = false)]
+        public string input;
+
+        [ValueArgument(typeof(string), 'c', "category", Description = "Category for this torrent", ValueOptional = false)]
+        public string category;
+
+        [ValueArgument(typeof(string), 'r', "root-path", Description = "Root path of the torrent", ValueOptional = false)]
+        public string rootPath;
+
+        [ValueArgument(typeof(string), 's', "save-path", Description = "Save path tof the torrent", ValueOptional = false)]
+        public string savePath;
+    }
+}
--- movieortv/Program.cs
+++ movieortv/Program.cs
@@ -8,6 +8,7 @@ using System.Net.Http;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using CommandLineParser;
 
 namespace movieortv
 {
@@ -15,15 +16,38 @@ namespace movieortv
     {
         static void Main(string[] args)
         {
-            string input = args[0];
+            CommandLineParser.CommandLineParser parser = new CommandLineParser.CommandLineParser();
+            Arguments arguments = new Arguments();
+
+            try
+            {
+                parser.ExtractArgumentAttributes(arguments);
+                parser.ParseCommandLine(args);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                parser.ShowUsage();
+                Environment.Exit(1);
+            }
+
+            string name = arguments.input;
+            string category = arguments.category;
+            string rootPath = arguments.rootPath;
+            string savePath = arguments.savePath;
+
             string pattern = @".+?(?=.(S[0-9]|[0-9]))";
             Regex reg = new Regex(pattern);
-            string searchTerm = reg.Match(input).ToString();
+            string searchTerm = reg.Match(name).ToString().Replace(".", " ");
 
-            RunAsync(searchTerm).Wait();
+
+            RunAsync(searchTerm, name, savePath, rootPath).Wait();
+
+            System.IO.File.WriteAllText(@"C:\Users\anton\Documents\output.txt", $"Torrent name = {name} \n Category = {category} \n Content path \n Root path = {rootPath} \n Save path = {savePath} \n Number of files =  \n Current tracker =  \n Info  hash = ");
+
         }
 
-        static async Task RunAsync(string searchTerm)
+        static async Task RunAsync(string searchTerm, string name, string savePath, string rootPath)
         {
             using (var client = new HttpClient())
             {
@@ -37,21 +61,36 @@ namespace movieortv
                 {
                     string stringItem = await response.Content.ReadAsStringAsync();
                     JToken token = JObject.Parse(stringItem);
-
+                    
                     string error = (string)token.SelectToken("Response");
 
                     if (error == "False")
                     {
-                        Console.WriteLine("Item not found!");
-                    } else
+                        Directory.CreateDirectory($"{savePath}unsorted");
+                        string destinationPath = $"{savePath}/unsorted/{name}";
+                        CopyDirectory(rootPath, destinationPath);
+                    }
+
+                    else
                     {
                         string something = (string)token.SelectToken("Type");
-                        Directory.CreateDirectory(something);
-                        IEnumerable<string> files = Directory.EnumerateFileSystemEntries("/");
+                        string destinationPath = $"{savePath}/{something}/{name}";
+                        Directory.CreateDirectory(destinationPath);
+                        CopyDirectory(rootPath, destinationPath);
                     }
-                    
                 }
             }
+        }
+
+        private static void CopyDirectory(string rootPath, string destinationPath)
+        {
+            foreach (string dirPath in Directory.GetDirectories(rootPath, "*",
+                SearchOption.AllDirectories))
+                Directory.CreateDirectory(dirPath.Replace(rootPath, destinationPath));
+
+            foreach (string newPath in Directory.GetFiles(rootPath, "*.*",
+                SearchOption.AllDirectories))
+                File.Copy(newPath, newPath.Replace(rootPath, destinationPath), true);
         }
     }
 }
